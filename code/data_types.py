f57b42da6a4624f9d74aab7e45dee9e45e7801a6
alejjara1 <alejandra2002.j@gmail.com>
2025-04-11 13:11:10 -0400

    determined epidermal depth

--- code/data_types.py
+++ code/data_types.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import scipy as sp
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 class Subject:
     def __init__(self, subject_id):
         self.subject_id = subject_id
         self.scans = None
         self.skin_roughness = None
-        self.epidermal = None
+        self.epidermal = []
         self.blood_flow = None
         self.num_scans = 0
         self.num_skin_roughness = 0
@@ -33,9 +35,6 @@ class Subject:
         """
         epidermal data function
         """
-        if self.epidermal is None:
-            self.epidermal = []
-
         self.epidermal.append(epidermal_data)
 
         self.num_epidermal += 1
@@ -53,7 +52,7 @@ class Subject:
 
     def add_scan_data(self, before_exposure_data, after_exposure_data):
         """
-        To add...
+        Get pre and post exposure information from excel sheets
         """
 
         self.before_scan_id = before_exposure_data["Scan#"].dropna()
@@ -113,6 +112,7 @@ class Epidermal:
         self.return_visit = return_visit
         self.min_value = None
         self.max_value = None
+        self.min_location_index = None
 
     def find_min_max(self):
         """
@@ -121,7 +121,7 @@ class Epidermal:
         self.min_value = self.depth_data["Mean A-scan"].min()
         self.max_value = self.depth_data["Mean A-scan"].max()
 
-    def normalize_a_scan(self):
+    def get_normalize_a_scan(self):
         """
         Normalize the Mean A-scan
         """
@@ -129,7 +129,7 @@ class Epidermal:
         self.depth_at_max_intensity = self.depth_data["Depth"][
             self.depth_data["Mean A-scan"].idxmax()
         ]
-        self.normalized_ascan = (self.depth_data["Mean A-scan"] - self.min_value) / (
+        self.normalized_a_scan = (self.depth_data["Mean A-scan"] - self.min_value) / (
             self.max_value - self.min_value
         )
 
@@ -138,7 +138,7 @@ class Epidermal:
         Calculate the Standard.....
         """
         value = 0.2
-        index = abs(self.normalized_ascan - value).idxmin()
+        index = abs(self.normalized_a_scan - value).idxmin()
         attenuation_depth = self.depth_data["Depth"].iloc[index]
         self.sid = 0.8 / attenuation_depth
 
@@ -148,36 +148,95 @@ class Epidermal:
         """
         integration_depth = 0.2
         lower_bound_index = self.depth_data["Mean A-scan"].idxmax()
-        
+
         upper_bound_index = abs(
             self.depth_data["Depth"] - (self.depth_at_max_intensity + integration_depth)
         ).idxmin()
 
         self.AuC = sp.integrate.trapezoid(
-            self.normalized_ascan[lower_bound_index:upper_bound_index],
+            self.normalized_a_scan[lower_bound_index:upper_bound_index],
             self.depth_data["Depth"][lower_bound_index:upper_bound_index],
         )
 
-
-    def plot_a_scan(self, subject_id,exposed):
+    def plot_a_scan(self, subject_id, exposed):
         """
         Plot the A-scan data
         """
-        fig, ax = plt.subplots()         
-        plt.plot(self.depth_data["Depth"], self.depth_data["Mean A-scan"], label="Mean A-Scan")
+        fig, ax = plt.subplots()
+        plt.plot(
+            self.depth_data["Depth"],
+            self.depth_data["Mean A-scan"],
+            label="Mean A-Scan",
+        )
+
+        if self.min_location_index is not None:
+            ax.plot(self.depth_data["Depth"][self.min_location_index],self.depth_data["Mean A-scan"][self.min_location_index],'x')
+
         plt.xticks(visible=False)
         if exposed:
-            plt.title(f'Subject id: {subject_id}  Scan id: {self.scan_id} Post-Exposure')
+            plt.title(
+                f"Subject id: {subject_id}  Scan id: {self.scan_id} Post-Exposure"
+            )
         else:
-            plt.title(f'Subject id: {subject_id}  Scan id: {self.scan_id} Pre-Exposure')
+            plt.title(f"Subject id: {subject_id}  Scan id: {self.scan_id} Pre-Exposure")
         plt.ylim(bottom=0)
         plt.ylabel("Intensity")
         plt.xlabel("Depth (mm)")
-        plt.xlim(0,2)
-        plt.legend() 
+        plt.xlim(0, 2)
+        plt.legend()
         # plt.savefig(f"../data_out/epidermal_data_out/subject_id_{subject_id}_scan_id_{self.scan_id}.pdf")
         # plt.close()
         plt.show()
+
+    def set_up_plot(self):
+        """Plot the derivative plots"""
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twinx()
+        ax1.plot(
+            self.depth_data["Depth"],
+            self.normalized_a_scan,
+            label="Mean A-Scan",
+        )
+
+    def compute_derivatives(self):
+        """Finding the first and second derivative."""
+        n_points = len(self.normalized_a_scan)
+        derivative = np.zeros_like(self.normalized_a_scan)
+        second_derivative = np.zeros_like(self.normalized_a_scan)
+
+        for n in range(1, n_points - 1):
+            h = self.depth_data["Depth"][n] - self.depth_data["Depth"][n - 1]
+            derivative[n] = (
+                self.normalized_a_scan[n + 1] - self.normalized_a_scan[n - 1]
+            ) / (self.depth_data["Depth"][n + 1] - self.depth_data["Depth"][n - 1])
+            second_derivative[n] = (
+                self.normalized_a_scan[n + 1]
+                - 2 * self.normalized_a_scan[n]
+                + self.normalized_a_scan[n - 1]
+            ) / (h * h)
+
+        return derivative, second_derivative
+
+    def approach_1(self, begin_skip):
+        """
+        Function for apporach 1 : derivative switches sign neg to pos
+        """
+        derivative, _ = self.compute_derivatives()
+        old_value = derivative[begin_skip]
+        for n in range(begin_skip + 1, 98):
+            new_value = derivative[n]
+            if old_value < 0 and new_value > 0:
+                self.min_location_index = n
+                break
+            else:
+                old_value = new_value
+    
+
+    def approach_2(self, begin_skip, end_skip):
+        """location of the largest change in the derivative."""
+        derivative, _ = self.compute_derivatives()
+        diff = np.diff(derivative[begin_skip : len(self.normalized_a_scan) - end_skip])
+        self.min_location_index = begin_skip + np.argmax(diff)
 
 
 class SkinRoughness:
